4c2d418b6add7bac85aa2d0c5be6b1f8b3a90cad
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2019-09-05 12:16:19 +0100

    Python tasks

---
+++ ACS Prog Tasks/name.py
@@ -0,0 +1,7 @@
+#Taking a name from the user
+
+name = input("Enter your name ")
+
+#Outputting "Hello" and user's name
+
+print("Hello", name)
---
+++ ACS Prog Tasks/times_table.py
@@ -0,0 +1,30 @@
+#Taking an integer input, 1 to 10, from user and outputting times table up to 12
+
+#using "a" for the for loop
+
+a = 0
+
+#Asking user for input
+
+num1 = int(input("Enter a number from 1 to 10 "))
+
+#While loop if number is outside 1,10 - retakes the input
+
+while num1 < 1 or num1 > 10:
+    
+    #if number is out of range: output error message, retake input
+    
+        print("Value out of range: re-enter value")
+        num1 = int(input())
+        
+    #if number is valid then outputs times tables
+        
+if num1 <= 10:
+    for a in range (1, 13):
+        print(num1, "x", a, "=", num1 * a)
+        a += 1
+        
+#endwhile
+            
+    
+
