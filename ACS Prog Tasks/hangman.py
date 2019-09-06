e884903b531d6530b074db45b375e05f6f21a44e
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2019-09-06 22:01:09 +0100

    hangman

--- ACS Prog Tasks/hangman.py
+++ ACS Prog Tasks/hangman.py
@@ -1,8 +1,8 @@
 count = 5
 max = 0
-list1 = []   #list of the input word to be guessed
+list_word2b_guessed = []   #list of the input word to be guessed
 list_start=[]
-list2 = []  #every letter guessed by the user
+list_guesses = []  #every letter guessed by the user2
 
 hangman_word = input("Player 1: Enter a word ")
 
@@ -11,7 +11,7 @@ length_word = len(hangman_word)
 #print("Word to guess:", "*" * length_word)
 
 for x in hangman_word:
-    list1.append(x)
+    list_word2b_guessed.append(x)
 
 for x in range (length_word):
     list_start.append("*")
@@ -22,16 +22,21 @@ while count > 0:
 
     user_guess = input("Enter guess - You have " + str(count) + " lives left ")
 
-    if user_guess in list1:
-        print("letter is in word")
-        index_user_guess = list1.index(user_guess)
-        list_start[index_user_guess]=user_guess
-        print(index_user_guess)
-        list2.append(user_guess)
+    if user_guess in list_word2b_guessed:
+        print("Letter is in word")
+        index_user_guess = list_word2b_guessed.index(user_guess)
+        list_start[index_user_guess] = user_guess
+        #print(index_user_guess)
+        if user_guess in list_guesses:
+            print("You've already guessed the letter " + user_guess)
+        else:
+            list_guesses.append(user_guess)
+            print(list_guesses)
+            print(list_start)
     else:
         count -= 1
-    print(list2)
-    print(list_start)
+  
+
 
 
 
