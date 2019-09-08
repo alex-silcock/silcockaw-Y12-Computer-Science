056b4a8c15a01972846384eba9804d2072527006
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2019-09-08 10:16:30 +0100

    Update hangman.py

--- ACS Prog Tasks/hangman.py
+++ ACS Prog Tasks/hangman.py
@@ -1,5 +1,5 @@
 count = 5
-max = 0
+
 list_word2b_guessed = []   #list of the input word to be guessed
 list_start=[]
 list_guesses = []  #every letter guessed by the user2
@@ -16,34 +16,27 @@ for x in hangman_word:
 for x in range (length_word):
     list_start.append("*")
 
-#double-letters: loop through list and if it exists more than once then output it however many times in the new list
+#double-letters: loop through the list_word2b_guessed and if a letter exists more than once, then both index values should be retrieved
 
 while count > 0:
 
     user_guess = input("Enter guess - You have " + str(count) + " lives left ")
-
     if user_guess in list_word2b_guessed:
         print("Letter is in word")
         index_user_guess = list_word2b_guessed.index(user_guess)
         list_start[index_user_guess] = user_guess
-        #print(index_user_guess)
-        if user_guess in list_guesses:
-            print("You've already guessed the letter " + user_guess)
-        else:
-            list_guesses.append(user_guess)
-            print(list_guesses)
-            print(list_start)
     else:
+        print("Letter is not in the word")
         count -= 1
-  
+        #print(index_user_guess)
 
+    if user_guess in list_guesses:
+        print("You've already guessed the letter " + user_guess)
+    else:
+        list_guesses.append(user_guess)
+        print(list_guesses)
+        print(list_start)
 
+ 
 
-
-
-
-
-
-
-
-
+        
