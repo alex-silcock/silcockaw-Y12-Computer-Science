33cc4848631589e8b1fb84b67d5790acf1bae247
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2019-09-09 17:45:42 +0100

    task 10

---
+++ ACS Prog Tasks/10 - Rock, paper, scissors.py
@@ -0,0 +1,45 @@
+#importing random to generate a random number from 1 to 3 to play against user_choice
+import random
+
+user_choice = input("Enter Rock(R), Paper(P) or Scissors(S) ")
+
+#while loop to make sure that the length of input is only 1 character long
+while len(user_choice) > 1:
+    print("Input not in correct format, try again.")
+    user_choice = input("Enter Rock(R), Paper(P) or Scissors(S) ")
+#end while
+
+computer_choice = random.randint(1,3)
+
+#if statement to convert computer choice to rock, paper or scissors
+if computer_choice == 1:
+    computer_choice = "Rock"
+elif computer_choice == 2:
+    computer_choice = "Paper"
+else: computer_choice = "Scissors"
+#end if
+
+print("The computer chose", computer_choice)
+
+#if statement to decide who wins, user or computer using the rules of rock, paper, scissors
+
+if user_choice == 'R' and computer_choice == "Rock":
+    print("Draw")
+elif user_choice == 'R' and computer_choice == "Paper":
+    print("You lose")
+elif user_choice == 'R' and computer_choice == "Scissors":
+    print("You win")
+elif user_choice == 'P' and computer_choice == "Paper":
+    print("Draw")
+elif user_choice == 'P' and computer_choice == "Rock":
+    print("You win")
+elif user_choice == 'P' and computer_choice == "Scissors":
+    print("You lose")
+elif user_choice == 'S' and computer_choice == "scissors":
+    print("Draw")
+elif user_choice == 'S' and computer_choice == "Rock":
+    print("You lose")
+elif user_choice == 'S' and computer_choice == "Paper":
+    print("You won")
+#end if
+
--- Kivy/main.py
+++ Kivy/main.py
@@ -1,11 +1,41 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+from kivy.uix.slider import Slider
+
+class MyGrid(GridLayout):
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 1
+
+        self.inside = GridLayout()
+        self.inside.cold = 2
+
+        self.inside.add_widget(Label(text="Name: "))
+        self.name = TextInput(multiline=False)
+        self.inside.add_widget(self.name)
+        
+        self.inside.add_widget(Label(text="Last Name: "))
+        self.lastname = TextInput(multiline=False)
+        self.inside.add_widget(self.lastname)
+
+        self.inside.add_widget(Label(text="Email: "))
+        self.email = TextInput(multiline=False)
+        self.inside.add_widget(self.email)
+
+        self.add_widget(self.inside)
+
+        self.submit = Button(text="Submit", font_size=40)
+        self.add_widget(self.submit)
 
 
 class MyApp(App):
     def build(self):
-        return Label(text="Tech with Alex")
+        return MyGrid()
+
 
 if __name__ == "__main__":
     MyApp().run()
