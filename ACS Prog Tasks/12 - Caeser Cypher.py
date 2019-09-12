140e1f95ddd276aa1e1f910172691d46263f3883
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2019-09-12 17:15:34 +0100

    Update 12 - Caeser Cypher.py

--- ACS Prog Tasks/12 - Caeser Cypher.py
+++ ACS Prog Tasks/12 - Caeser Cypher.py
@@ -1,5 +1,15 @@
 user_input = input("Enter word ")
 
+plain_text = []
+
+plain_text.append(list(user_input))
+
+for count in range(1, len(user_input)):
+    chr(plain_text[count])
+
+
+
+
 #ascii_input = ord(user_input)
 #ascii_input += 2
 
