8afabb3337dbe03f8486765981cf2ff9032e2f53
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2019-10-16 22:22:20 +0100

    Main Menu

--- Pygame/pong_4.py
+++ Pygame/pong_4.py
@@ -1,43 +1,14 @@
 # -- library imports
 import pygame
-import pygameMenu
-import pygameMenu.config as _cfg
-import pygameMenu.controls as _ctrl
-import pygameMenu.events as _events
-import pygameMenu.font as _fonts
-import pygameMenu.locals as _locals
-import pygameMenu.widgets as _widgets
 
 # -- Global Constants
-ball_width = 20
-x_val = 150
-y_val = 200
-x_direction = 5
-y_direction = 5
-padd_length = 15
-padd_width = 60
-x_padd = 0
-y_padd = 200
-x_padd_2 = 625
-y_padd_2 = 200
-score_a = 0
-score_b = 0
 
-def draw_score_a(screen, x, y, score):
-    font = pygame.font.SysFont("arial", 20)
-    text_a = font.render(str(score_a), 1, WHITE)
-    screen.blit(text_a, (x, y))
-#enddef
-def draw_score_b(screen, x, y, score):
-    font = pygame.font.SysFont("arial", 20)
-    text_b = font.render(str(score_b), 1, WHITE)
-    screen.blit(text_b, (x, y))
-#enddef
-
-def message(screen, x, y):
+def message(screen):
     font=pygame.font.SysFont("arial", 20)
     text = font.render("Pong Game (C) Alex Silcock", 1, WHITE)
-    screen.blit(text, (x, y))
+    textRect = text.get_rect()
+    textRect.center = (size[0]//2,size[1]//2)
+    screen.blit(text, textRect)
 #enddef
 
 def text_format(message, textFont, textSize, textColor):
@@ -61,132 +32,193 @@ screen = pygame.display.set_mode(size)
 # -- Title of new window/screen
 pygame.display.set_caption("Pong")
 
-# -- Exit game flag set to false
-done = False
-
 # -- Manages how fast screen refreshes
 clock = pygame.time.Clock()
 
-while not done:
+#defining the function of ponggame() to then overlay menus
+def pong_game():
+    ball_width = 20
+    x_val = 150
+    y_val = 200
+    x_direction = 5
+    y_direction = 5
+    padd_length = 15
+    padd_width = 60
+    x_padd = 0
+    y_padd = 200
+    x_padd_2 = 625
+    y_padd_2 = 200
+    score_a = 0
+    score_b = 0
+    done = False
+    
+    def draw_score_a(screen, x, y, score):
+        font = pygame.font.SysFont("arial", 20)
+        text_a = font.render(str(score_a), 1, WHITE)
+        screen.blit(text_a, (x, y))
+    #end function
 
-    # -- User input and controls
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
-        #End If
-        
-    keys = pygame.key.get_pressed()
-    # -- the up key or down key has been pressed
-    if keys[pygame.K_UP]:
-        # -- write logic that happens on key press here
-        y_padd = y_padd - 5
-        if y_padd < 0:
-            y_padd = 0
-    elif keys[pygame.K_DOWN]:
-        y_padd = y_padd + 5
-        if y_padd > (size[1] - padd_length):
-            y_padd = (size[1] - padd_length)
-                # -- write logic that happens on key press here
+    def draw_score_b(screen, x, y, score):
+        font = pygame.font.SysFont("arial", 20)
+        text_b = font.render(str(score_b), 1, WHITE)
+        screen.blit(text_b, (x, y))
+    #end function
+
+    while not done:
+
+        # -- User input and controls
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
             #End If
-        #End If
-    keys = pygame.key.get_pressed()
-    # -- the w(up) key or s(down) key has been pressed
-    if keys[pygame.K_w]:
-        # -- write logic that happens on key press here
-        y_padd_2 = y_padd_2 - 5
-    elif keys[pygame.K_s]:
-        y_padd_2 = y_padd_2 + 5
+            
+        keys = pygame.key.get_pressed()
+        # -- the up key or down key has been pressed
+        if keys[pygame.K_UP]:
             # -- write logic that happens on key press here
+            y_padd = y_padd - 5
+            if y_padd < 0:
+                y_padd = 0
+        elif keys[pygame.K_DOWN]:
+            y_padd = y_padd + 5
+            if y_padd > (size[1] - padd_length):
+                y_padd = (size[1] - padd_length)
+                    # -- write logic that happens on key press here
+                #End If
             #End If
-        #End If
-    #Next event
+        keys = pygame.key.get_pressed()
+        # -- the w(up) key or s(down) key has been pressed
+        if keys[pygame.K_w]:
+            # -- write logic that happens on key press here
+            y_padd_2 = y_padd_2 - 5
+        elif keys[pygame.K_s]:
+            y_padd_2 = y_padd_2 + 5
+                # -- write logic that happens on key press here
+                #End If
+            #End If
+        #Next event
 
-    # -- Game logic goes after this comment
-    #ball movement
-    x_val = x_val + x_direction
-    y_val = y_val + y_direction
-    #Making the right paddle move
-    if y_padd_2 > y_val:
-        y_padd_2 = y_padd_2 - 6
-    elif y_padd_2 < y_val:
-        y_padd_2 = y_padd_2 + 6
-    #End if
+        # -- Game logic goes after this comment
+        #ball movement
+        x_val = x_val + x_direction
+        y_val = y_val + y_direction
+
+        #Making the right paddle move
+        if y_padd_2 > y_val:
+            y_padd_2 = y_padd_2 - 6
+        elif y_padd_2 < y_val:
+            y_padd_2 = y_padd_2 + 6
+        #End if
     
-    #making the ball "bounce"
-    if x_val > (size[0] - ball_width):
-       x_direction = x_direction * -1
-    #endif
+        #if the ball goes past and the paddle hasnt hit it, then it will be reset, and the score of player b will change
+        #as the opposite player has won a point
+        if x_val < 0:
+            clock.tick(1)
+            x_val = 600
+            y_val = 200
+            x_direction = -5
+            y_direction = 5
+            score_b = score_b + 1
+            
+        #endif
 
-    #if the ball goes past and the paddle hasnt hit it, then it will be reset, and the score of player b will change
-    #as the opposite player has won a point
-    if x_val < 0:
-        x_val = 150
-        y_val = 200
-        x_direction = 5
-        y_direction = 5
-        score_b = score_b + 1
-        x_padd = 0
-        y_padd = 200
-        x_padd_2 = 625
-        y_padd_2 = 200
-    #endif
+        #doing the above but for the opposite player
+        if x_val > 640 - ball_width:
+            clock.tick(1)
+            x_val = 40
+            y_val = 200
+            x_direction = 5
+            y_direction = 5
+            score_a = score_a + 1
+            
+        #endif
 
-    #doing the above but for the opposite player
-    if x_val > 640 - ball_width:
-        x_val = 150
-        y_val = 200
-        x_direction = 5
-        y_direction = 5
-        score_a = score_a + 1
-        x_padd = 0
-        y_padd = 200
-        x_padd_2 = 625
-        y_padd_2 = 200
-    #endif
+        #max score, if the score is 5 then the game will end
+        if score_a == 5 or score_b == 5:
+            done = True
+        #endif
 
-    #max score, if the score is 5 then the game will end
-    if score_a == 5 or score_b == 5:
-        done = True
-    #endif
+        if y_val > (size[1] - ball_width) or y_val < 0:
+            y_direction = y_direction * -1
+        #endif
 
-    if y_val > (size[1] - ball_width) or y_val < 0:
-       y_direction = y_direction * -1
-    #endif
+        #collisions for left paddle, changes direction + speeds up
+        if x_val < padd_length:
+            if y_val > y_padd - (ball_width//2) and y_val < (y_padd + padd_width):  
+                x_direction = 5
+        #endif
+        #collisions for right paddle
+        if x_val > size[0] - (padd_width - ball_width):
+            if y_val > y_padd_2 - (ball_width//2) and y_val < (y_padd_2 + padd_width):  
+                x_direction = abs(x_direction) * -1
+        #endif
 
-    #collisions for left paddle, changes direction + speeds up
-    if (y_val <= y_padd + padd_length and y_val >= y_padd) and x_val <= padd_width:
-        x_direction = x_direction * -1.1
-        #x_direction = x_direction + 20
-    #endif
-    #collisions for right paddle
-    if (y_val <= y_padd_2 + padd_length and y_val >= y_padd_2) and x_val >= (size[0] - padd_width):
-        x_direction = x_direction * -1.1
-        #x_direction = x_direction + 20
-    #endif
+        # -- Screen background is BLACK
+        screen.fill (BLACK)
+
+        # -- Draw here
+        pygame.draw.rect(screen, BLUE, (x_val, y_val, ball_width, ball_width))
+        pygame.draw.rect(screen, WHITE, (x_padd, y_padd, padd_length, padd_width))
+        draw_score_a(screen, 300, 30, score_a)
+        draw_score_b(screen, 330, 30, score_b)
+        pygame.draw.rect(screen, WHITE, (x_padd_2, y_padd_2, padd_length, padd_width))
+        message(screen)
+        
+
+        # -- flip display to reveal new position of objects
+        pygame.display.flip()
+
+        # - The clock ticks over
+        clock.tick(60)
+
+    #End While - End of game loop
+#endfunction
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, BLACK)
+    return textSurface, textSurface.get_rect()
+#end function
 
 
-    if x_val > size[0] - padd_width:
-        x_val = size[0] - padd_width
-    #endif
+def main_menu():
 
-    # -- Screen background is BLACK
-    screen.fill (BLACK)
-
-    # -- Draw here
-    pygame.draw.rect(screen, BLUE, (x_val, y_val, ball_width, ball_width))
-    pygame.draw.rect(screen, WHITE, (x_padd, y_padd, padd_length, padd_width))
-    draw_score_a(screen, 300, 30, score_a)
-    draw_score_b(screen, 330, 30, score_b)
-    pygame.draw.rect(screen, WHITE, (x_padd_2, y_padd_2, padd_length, padd_width))
-    message(screen, (size[0] / 2), (size[1] / 2))
+    intro = True
+    #Font's and text
+    font = pygame.font.SysFont ("arial", 60)
+    text = font.render ("", True, WHITE)
     
 
-    # -- flip display to reveal new position of objects
+    #Background Image
+    screen.fill(BLUE)
+    screen.blit(text, (size[0]//2, size[1]//2))
+
+    #BUTTONS
+    screen.blit(text, (size[0]//2 , size[1]//2))
+    pygame.draw.rect(screen,(0,0,0),(200, 200, 205,80))
+    pygame.draw.rect(screen,(0,0,0),(300,400,205,80))
+    pygame.draw.rect(screen,(0,0,0),(300,500,205,80))
+
+    font = pygame.font.SysFont ("arial", 30)
+    text = font.render ("START", True, WHITE)
+    screen.blit(text, (250, 250))
+    font = pygame.font.SysFont ("arial", 30)
+    text = font.render ("OPTIONS", True, WHITE)
+    screen.blit(text, (340, 420))
+    font = pygame.font.SysFont ("arial", 30)
+
     pygame.display.flip()
+    
+    while intro:
+        for event in pygame.event.get():
+            #print(event)
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+            #end if
+        #next event
+    #end while
 
-    # - The clock ticks over
-    clock.tick(60)
 
-#End While - End of game loop
-
+main_menu()
+pong_game()
 pygame.quit()
