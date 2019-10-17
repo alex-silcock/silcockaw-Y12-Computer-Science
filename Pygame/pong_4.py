f25813cbf7600391487f3291d21056e3482eada0
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2019-10-17 20:51:34 +0100

    Main menu updates

--- Pygame/pong_4.py
+++ Pygame/pong_4.py
@@ -1,6 +1,5 @@
 # -- library imports
 import pygame
-import pygame.font
 
 # -- Global Constants
 # -- Colours
@@ -40,6 +39,8 @@ def text_format(message, textFont, textSize, textColor):
 def main_menu():
     finished = False
     textcolour = BLACK
+    textcolour_2 = BLACK
+
     while not finished:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -53,15 +54,21 @@ def main_menu():
         # Background Image
         screen.fill(WHITE)
 
+        mouse = pygame.mouse.get_pos()
+
         font = pygame.font.SysFont("helveltica", 80)
         text = font.render("PLAY", 5, textcolour)
-        mouse = pygame.mouse.get_pos()
-        
-        # BUTTONS
-
         textRect = text.get_rect()
         textRect.center = (size[0]//2, size[1]//2)
         screen.blit(text, textRect)
+
+        font_2 = pygame.font.SysFont("helveltica", 80)
+        text_2 = font.render("MULTIPLAYER", 5, textcolour_2)
+        textRect_2 = text_2.get_rect()
+        textRect_2.center = (size[0]//2, 320)
+        screen.blit(text_2, textRect_2)
+        
+        # BUTTONS
         if textRect.collidepoint(mouse):
             textcolour = RED
         else:
@@ -76,11 +83,25 @@ def main_menu():
                 #end if
             #end if
         #next event
+
+        if textRect_2.collidepoint(mouse):
+            textcolour_2 = RED
+        else:
+            textcolour_2 = BLACK
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                if textRect_2.collidepoint(mouse):
+                    finished = True
+                #end if
+            #end if
+        #next event 
     #end while
 # end function
 
-
-
+#========================================= pong singleplayer =============================================#
 def pong_game():
     
     ball_width = 20
@@ -119,6 +140,7 @@ def pong_game():
 
     # ============ end of defining functions ============ #
     game_over = False
+    ball_color = BLUE
     while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -127,9 +149,7 @@ def pong_game():
         #End If
     
         keys = pygame.key.get_pressed()
-        # -- the up key or down key has been pressed
         if keys[pygame.K_UP]:
-         # -- write logic that happens on key press here
             left_y = left_y - 5
             if left_y < 0:
                 left_y = 0
@@ -144,6 +164,7 @@ def pong_game():
         #End If
 
         # -- Game logic goes after this comment
+
         # ball movement
         x_val = x_val + x_direction
         y_val = y_val + y_direction
@@ -180,28 +201,32 @@ def pong_game():
         if score_a == 5 or score_b == 5:
             game_over = False
             return game_over
-        # endif
+        #endif
 
+        #making ball bounce
         if y_val > (size[1] - ball_width) or y_val < 0:
             y_direction = y_direction * -1
         # endif
-
+        
         # collisions for left paddle, changes direction + speeds up
         if x_val < padd_width:
             if y_val > left_y - (ball_width//2) and y_val < (left_y + padd_height):
                 x_direction = x_direction * -1.05
-        # endif
+                ball_color = RED
+            #end if
+        #end if
         # collisions for right paddle
         if x_val > size[0] - (padd_height - ball_width):
             if y_val > right_y - (ball_width//2) and y_val < (right_y + padd_height):
                 x_direction = abs(x_direction) * -1.05
+                ball_color = YELLOW
         # endif
 
         # -- Screen background is BLACK
         screen.fill(BLACK)
 
         # -- Draw here
-        pygame.draw.rect(screen, BLUE, (x_val, y_val, ball_width, ball_width))
+        pygame.draw.rect(screen, ball_color, (x_val, y_val, ball_width, ball_width))
         pygame.draw.rect(screen, WHITE, (left_x, left_y, padd_width, padd_height))
         draw_score_a(screen, size[0]//2 - 20, 30, score_a)
         draw_score_b(screen, size[0]//2 + 20, 30, score_b)
@@ -213,8 +238,150 @@ def pong_game():
         clock.tick(60)
 #end function
 
+#========================================== pong multiplayer===============================================#
 
+def pong_game_multiplayer():
+    
+    ball_width = 20
+    x_val = 150
+    y_val = 200
+    speed = 5
+    x_direction = speed
+    y_direction = speed
+    padd_width = 15
+    padd_height = 60
+    left_x = 0
+    left_y = 200
+    right_x = 625
+    right_y = 200
+    score_a = 0
+    score_b = 0
 
+    def draw_score_a(screen, x, y, score):
+        font = pygame.font.SysFont("arial", 20)
+        text_a = font.render(str(score_a), 1, WHITE)
+        screen.blit(text_a, (x, y))
+    # end function
+
+    def draw_score_b(screen, x, y, score):
+        font = pygame.font.SysFont("arial", 20)
+        text_b = font.render(str(score_b), 1, WHITE)
+        screen.blit(text_b, (x, y))
+    # end function
+
+    def text_objects(text, font):
+        textSurface = font.render(text, True, BLACK)
+        return textSurface, textSurface.get_rect()
+    # end function
+
+    
+
+    # ============ end of defining functions ============ #
+    game_over = False
+    ball_color = BLUE
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+                return game_over
+            #End If
+        #next event
+    
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            left_y = left_y - 5
+            if left_y < 0:
+                left_y = 0
+        elif keys[pygame.K_DOWN]:
+            left_y = left_y + 5
+        
+        #for second player
+
+        if keys[pygame.K_w]:
+            right_y = right_y - 5
+            if right_y < 0:
+                right_y = 0
+        elif keys[pygame.K_s]:
+            right_y = right_y + 5
+        # -- write logic that happens on key press here
+        #End If
+        
+        if left_y > (size[1] - padd_height):
+            left_y = size[1] - padd_height
+        elif right_y > (size[1] - padd_height):
+            right_y = size[1] - padd_height
+            #End If
+        #End If
+
+        # -- Game logic goes after this comment
+
+        # ball movement
+        x_val = x_val + x_direction
+        y_val = y_val + y_direction
+
+        # if the ball goes past and the paddle hasn't hit it, then it will be reset, and the score of player b will change
+        # as the opposite player has won a point
+        if x_val < 0:
+            clock.tick(1)
+            x_val = 600
+            y_val = 200
+            x_direction = speed * -1
+            y_direction = speed
+            score_b = score_b + 1
+        # endif
+
+        # doing the above but for the opposite player
+        if x_val > 640 - ball_width:
+            clock.tick(1)
+            x_val = 40
+            y_val = 200
+            x_direction = speed
+            y_direction = speed
+            score_a = score_a + 1
+        # endif
+
+        # max score, if the score is 5 then the game will end
+        if score_a == 5 or score_b == 5:
+            game_over = False
+            return game_over
+        #endif
+
+        #making ball bounce
+        if y_val > (size[1] - ball_width) or y_val < 0:
+            y_direction = y_direction * -1
+        # endif
+        
+        # collisions for left paddle, changes direction + speeds up
+        if x_val < padd_width:
+            if y_val > left_y - (ball_width//2) and y_val < (left_y + padd_height):
+                x_direction = x_direction * -1.05
+                ball_color = RED
+            #end if
+        #end if
+        # collisions for right paddle
+        if x_val > size[0] - (padd_height - ball_width):
+            if y_val > right_y - (ball_width//2) and y_val < (right_y + padd_height):
+                x_direction = abs(x_direction) * -1.05
+                ball_color = YELLOW
+        # endif
+
+        # -- Screen background is BLACK
+        screen.fill(BLACK)
+
+        # -- Draw here
+        pygame.draw.rect(screen, ball_color, (x_val, y_val, ball_width, ball_width))
+        pygame.draw.rect(screen, WHITE, (left_x, left_y, padd_width, padd_height))
+        draw_score_a(screen, size[0]//2 - 20, 30, score_a)
+        draw_score_b(screen, size[0]//2 + 20, 30, score_b)
+        pygame.draw.rect(screen, WHITE, (right_x, right_y, padd_width, padd_height))
+        message(screen)
+
+        # -- flip display to reveal new position of objects
+        pygame.display.flip()
+        clock.tick(60)
+#end function
+
+# ====== Main Program ====== #
 in_game = False
 game_over = False
 
--- Pygame/pong_additional_code.py
+++ Pygame/pong_additional_code.py
@@ -9,3 +9,31 @@ elif keys[pygame.K_s]:
     y_padd_2 = y_padd_2 + 5
 # -- write logic that happens on key press here
 #End If
+
+
+#to have moving balls on the front screen
+    first_cube_x = 100
+    first_cube_y = 100
+    second_cube_x = 100
+    second_cube_y = 320
+    third_cube_x = 100
+    third_cube_y = 392
+    fourth_cube_x = 100
+    fourth_cube_y = 420
+
+    x_direction = 5
+    y_direction = 5
+
+    pygame.draw.rect(screen, BLACK, (first_cube_x, first_cube_x, 10, 10))
+    pygame.draw.rect(screen, BLACK, (second_cube_x, second_cube_y, 10, 10))
+    pygame.draw.rect(screen, BLACK, (third_cube_x, third_cube_y, 10, 10))
+    pygame.draw.rect(screen, BLACK, (fourth_cube_x, fourth_cube_y, 10, 10))
+
+    first_cube_x = first_cube_x + x_direction
+    first_cube_y = first_cube_y + y_direction
+    second_cube_x = second_cube_x + x_direction
+    second_cube_y = second_cube_y + y_direction
+    third_cube_x = third_cube_x + x_direction
+    third_cube_y = third_cube_y + y_direction
+    fourth_cube_x = fourth_cube_x + x_direction
+    fourth_cube_y = fourth_cube_y + y_direction
