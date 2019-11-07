74900c133be3ec9ac7476395985aecc30c5dd73a
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2019-11-07 12:28:26 +0000

    learninformatics

---
+++ Pygame/invaders.py
@@ -0,0 +1,112 @@
+import pygame
+import random
+import math
+
+# -- Global Constants
+
+## -- Define the class snow which is a sprite
+class Invader(pygame.sprite.Sprite):
+    # Define the constructor for snow
+    def __init__(self, color, width, height,speed):
+        # Call the sprite constructor
+        super().__init__()
+        # Create a sprite and fill it with colour
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        # Set the position of the sprite
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, 600)
+        self.rect.y = random.randrange(-50, 0)
+        self.speed = speed
+    #End Procedure
+
+    # Class update function - runs for each pass through the game loop
+    def update(self):
+        self.rect.y = self.rect.y + self.speed
+    #end func
+#End Class
+class Player(pygame.sprite.Sprite):
+    # Define the constructor for snow
+    def __init__(self, color, width, height,speed):
+        # Call the sprite constructor
+        super().__init__()
+        # Create a sprite and fill it with colour
+        self.image = pygame.Surface([width,height])
+        self.image.fill(color)
+        # Set the position of the sprite
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0, 600)
+        self.rect.y = random.randrange(-50, 0)
+        self.speed = 0
+    #end func
+
+    # Class update function - runs for each pass through the game loop
+    def update(self):
+        self.rect.y = self.rect.y + self.speed
+    #end func
+#end class
+
+# -- Colours
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (50,50,255)
+YELLOW = (255,255,0)
+
+# -- Initialise PyGame
+pygame.init()
+
+# -- Blank Screen
+size = (640,480)
+screen = pygame.display.set_mode(size)
+
+# -- Title of new window/screen
+pygame.display.set_caption("Invader")
+
+# -- Exit game flag set to false
+done = False
+
+# Create a list of the snow blocks
+invader_group = pygame.sprite.Group()
+
+# Create a list of all sprites
+all_sprites_group = pygame.sprite.Group()
+
+# -- Manages how fast screen refreshes
+clock = pygame.time.Clock()
+
+# Create the invaders
+number_of_invaders = 10
+for x in range (number_of_invaders):
+    my_invader = Invader(BLUE, 10, 10, 1)
+    invader_group.add (my_invader)
+    all_sprites_group.add (my_invader) # adds it to the group of all Sprites
+#Next x
+
+
+while not done:
+    # -- User input and controls
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+        #End If
+    #Next event
+
+    # -- Game logic goes after this comment
+    all_sprites_group.update()
+
+    # -- Screen background is BLACK
+    screen.fill (BLACK)
+    all_sprites_group.draw(screen)
+
+    # -- Draw here
+
+    # -- flip display to reveal new position of objects
+    pygame.display.flip()
+
+    # - The clock ticks over
+    clock.tick(60)
+
+#End While - End of game loop
+
+pygame.quit()
