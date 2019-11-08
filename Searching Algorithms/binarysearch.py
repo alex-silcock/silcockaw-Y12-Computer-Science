9eab685cd6f13ccf0f3d395ade65d968b73c785c
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2019-11-08 13:10:59 +0000

    searches

---
+++ Searching Algorithms/binarysearch.py
@@ -0,0 +1,28 @@
+namelist = ["Amelia", "Ava", "Brave", "Krystal", "Precious", "Wonderful"]
+
+def binarySearch(namelist, itemSought):
+    found = False
+    index = -1
+    first = 0
+    last = len(namelist)-1
+    while first <= last and not found:
+        midpoint = int((first + last) / 2)
+        if namelist[midpoint] == itemSought:
+            found = True
+            index = midpoint
+        else:
+            if namelist[midpoint] < itemSought:
+                first = midpoint + 1
+            else:
+                last = midpoint - 1
+            #end if
+        #end if
+    #end while
+    return index
+#end function
+
+# main prog
+
+nameSought = input("Enter name for searching ")
+index_of_name = binarySearch(namelist, nameSought)
+print("Position of name : " , index_of_name + 1)
---
+++ Searching Algorithms/linearsearch.py
@@ -0,0 +1,21 @@
+namelist = ["Amelia", "Ava", "Brave", "Krystal", "Precious", "Wonderful"]
+
+def linearSearch(namelist, nameSought):
+    index = -1
+    count = 0
+    found = False
+    while count < len(namelist) and not found:
+        if namelist[count] == nameSought:
+            index = count
+            found = True
+        #end if
+        count = count + 1
+    #end while
+    return index
+#end function
+
+# main prog
+
+nameSought = input("Enter name for searching ")
+index_of_name = linearSearch(namelist, nameSought)
+print("Position of name : " , index_of_name + 1)
