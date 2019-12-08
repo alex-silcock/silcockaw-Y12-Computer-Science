c3adcd39b3d32b4783b18b2b0caeba53e5d725af
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2019-12-08 08:22:08 +0000

    Update game.py

--- A-Level Project/game.py
+++ A-Level Project/game.py
@@ -11,8 +11,9 @@
 
 # -- Library imports
 import pygame
+import random
 
-# -- Global constants, functions, procedures, classes
+# -- Global constants
 
 # -- Colours
 BLACK = (0, 0, 0)
@@ -39,11 +40,56 @@ clock = pygame.time.Clock()
 # Set game loop to false so it runs
 done = False
 
+# Creating the sprite groups
+player_group = pygame.sprite.Group()
+platform_group = pygame.sprite.Group()
+bullett_group = pygame.sprite.Group()
+all_sprites_group = pygame.sprite.Group()
+
 # Create an object using the class
 class Player(pygame.sprite.Sprite):
     # Define the constructor for the player
     def __init__(self):
-        self.x = 100
+        # Call the sprite constructor
+        super().__init__()
+        self.image = pygame.image.load("invader.jpg")
+        self.rect = self.image.get_rect()
+        self.rect.x = platform.rect.x
+        self.rect.y = 100
+        self.lives = 100
+    #end procedure
+
+    # Class methods
+    def update(self):
+        self.rect.x += 1
+    #end procedure
+#end class
+class Platform(pygame.sprite.Sprite):
+    # Define the constructor for the player
+    def __init__(self):
+        # Call the sprite constructor
+        super().__init__()
+        width = random.randrange(100)
+        height = 20
+        self.image = pygame.Surface([width,height])
+        self.image.fill(RED)
+        # Set the position of the sprite
+        self.rect = self.image.get_rect()
+        self.rect.x = 10
+        self.rect.y = 780
+    #end procedure
+#end class
+
+
+player = Player()
+player_group.add(player)
+all_sprites_group.add(player)
+
+for x in range(10):
+    platform = Platform()
+    platform_group.add(platform)
+    all_sprites_group.add(platform)
+
 
 # Game loop
 while not(done):
@@ -53,8 +99,12 @@ while not(done):
         #End If
     #Next event
 
+
+
+    all_sprites_group.update()
     # Clear the screen
     screen.fill(BLACK)
+    all_sprites_group.draw(screen)
     
 
     # Limit to 60 FPS
