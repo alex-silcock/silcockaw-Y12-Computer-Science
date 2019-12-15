195b6d13719f8bb55b8aebecf774af0dbdd71cd7
alex-silcock <54852303+alex-silcock@users.noreply.github.com>
2019-12-15 19:09:22 +0000

    updates

--- Pygame/Maze/pyt.py
+++
@@ -1,9 +0,0 @@
-import json
-from Pygame.Maze.mazeAlgos import getNodes
-
-file = open("Python/Shortest paths/map.JSON", "r")
-maze = json.load(file)
-file.close()
-
-a = getNodes(maze)
-print(a)
--- Pygame/Maze/test.py
+++
@@ -1,8 +0,0 @@
-from mazeAlgos import getNodes
-import json
-file = open("Pygame/Shortest paths/map.JSON", "r")
-maze = json.load(file)
-file.close()
-
-a = getNodes(maze)
-print(a)
